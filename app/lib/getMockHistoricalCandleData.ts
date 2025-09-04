27daa24f6689ccd49659dd917242f70c305bae0e
sjbao <supuilam@gmail.com>
2025-09-04 15:43:31 +0900

    fix initial chart data interval

--- app/lib/getMockHistoricalCandleData.ts
+++ app/lib/getMockHistoricalCandleData.ts
@@ -2,10 +2,10 @@ import { UTCTimestamp } from "lightweight-charts";
 import { CandleMsg } from "../components/WebSocketProvider";
 
 export function getMockHistoricalCandleData(): CandleMsg['candle'][] {
-  const now = Date.now()
+  const now = new Date().getTime();
   return Array.from({ length: 40 }, (_, i) => ({
     // interval 1 minute apart
-    time: (now - (i + 1 * 60 * 1000)) / 1000 as UTCTimestamp,
+    time: (now / 1000) + (-i * 60) as UTCTimestamp,
     open: 100 + Math.random() * 1,
     high: 101 + Math.random() * 1,
     low: 99 + Math.random() * 1,
